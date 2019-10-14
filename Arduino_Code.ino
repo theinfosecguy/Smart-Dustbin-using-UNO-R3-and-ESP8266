69ad657108479bb6b375ba34cb779ff0a12abd7a
Dhawal Singh Panwar <dsp9107@outlook.com>
2019-10-14 23:57:52 +0530

    Made More Manageable And Readable :D

--- Arduino_Code.ino
+++ Arduino_Code.ino
@@ -1,37 +1,35 @@
 #include <Servo.h>
-Servo servoMain; // Define our Servo
-int trigpin = 10;
-int echopin = 11;
-int distance;
-float duration;
-float cm;
+
+Servo servoMain;  // Define our Servo
+int servopin = 9, trigpin = 10, echopin = 11, distance = 0;
+float duration = 0.0, cm = 0.0;
 
 void setup()
 {
-servoMain.attach(9); // servo on digital pin 9
-pinMode(trigpin, OUTPUT);
-pinMode(echopin, INPUT);
+  servoMain.attach(servopin);
+  pinMode(trigpin, OUTPUT);
+  pinMode(echopin, INPUT);
 }
 
 void loop()
-{ 
-digitalWrite(trigpin, LOW);
-delay(100); 
-digitalWrite(trigpin, HIGH);
-delayMicroseconds(10); 
-digitalWrite(trigpin, LOW); 
-duration = pulseIn(echopin, HIGH); 
-cm = (duration/58.82);
-distance = cm;
-
-if(distance<30)
 {
-servoMain.write(180); // Turn Servo back to center position (90 degrees)
-delay(3000); 
-}
-else{
-servoMain.write(0);
-delay(100);
-}
+  digitalWrite(trigpin, LOW);
+  delay(100); 
+  digitalWrite(trigpin, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(trigpin, LOW); 
+  duration = pulseIn(echopin, HIGH); 
+  cm = (duration / 58.82);
+  distance = cm;
 
+  if(distance < 30)
+  {
+    servoMain.write(180); // Turn Servo back to center position (90 degrees)
+    delay(3000); 
+  }
+  else
+  {
+    servoMain.write(0);
+    delay(100);
+  }
 }
